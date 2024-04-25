5b317b2e1504dc979e09fc228a1989e410cd89be
Tiago Costa <costa.tiago@gmail.com>
2024-04-25 18:49:47 +0200

    Implement Chapter 1 - Lexing

---
+++ lexer/lexer.go
@@ -0,0 +1,139 @@
+package lexer
+
+import (
+	"monkey-interpreter/token"
+)
+
+type Lexer struct {
+	input        string
+	position     int
+	readPosition int
+	char         byte
+}
+
+func New(input string) *Lexer {
+	lexer := &Lexer{input: input}
+	lexer.readChar()
+	return lexer
+}
+
+func (lexer *Lexer) NextToken() token.Token {
+	var tok token.Token
+
+	lexer.skipWhitespace()
+
+	switch lexer.char {
+	case '=':
+		if lexer.peekChar() == '=' {
+			char := lexer.char
+			lexer.readChar()
+			tok = token.Token{Type: token.EQ, Literal: string(char) + string(lexer.char)}
+		} else {
+			tok = newToken(token.ASSIGN, lexer.char)
+		}
+	case '+':
+		tok = newToken(token.PLUS, lexer.char)
+	case '-':
+		tok = newToken(token.MINUS, lexer.char)
+	case '!':
+		if lexer.peekChar() == '=' {
+			char := lexer.char
+			lexer.readChar()
+			tok = token.Token{Type: token.NOT_EQ, Literal: string(char) + string(lexer.char)}
+		} else {
+			tok = newToken(token.BANG, lexer.char)
+		}
+	case '/':
+		tok = newToken(token.SLASH, lexer.char)
+	case '*':
+		tok = newToken(token.ASTERISK, lexer.char)
+	case '<':
+		tok = newToken(token.LT, lexer.char)
+	case '>':
+		tok = newToken(token.GT, lexer.char)
+	case ';':
+		tok = newToken(token.SEMICOLON, lexer.char)
+	case ',':
+		tok = newToken(token.COMMA, lexer.char)
+	case '(':
+		tok = newToken(token.LPAREN, lexer.char)
+	case ')':
+		tok = newToken(token.RPAREN, lexer.char)
+	case '{':
+		tok = newToken(token.LBRACE, lexer.char)
+	case '}':
+		tok = newToken(token.RBRACE, lexer.char)
+	case 0:
+		tok.Literal = ""
+		tok.Type = token.EOF
+	default:
+		if isLetter(lexer.char) {
+			tok.Literal = lexer.readIdentifier()
+			tok.Type = token.LookupIdent(tok.Literal)
+			return tok
+		} else if isDigit(lexer.char) {
+			tok.Type = token.INT
+			tok.Literal = lexer.readNumber()
+			return tok
+		} else {
+			tok = newToken(token.ILLEGAL, lexer.char)
+		}
+	}
+
+	lexer.readChar()
+	return tok
+}
+
+func (lexer *Lexer) readChar() {
+	if lexer.readPosition >= len(lexer.input) {
+		lexer.char = 0
+	} else {
+		lexer.char = lexer.input[lexer.readPosition]
+	}
+	lexer.position = lexer.readPosition
+	lexer.readPosition++
+}
+
+func (lexer *Lexer) readIdentifier() string {
+	position := lexer.position
+	for isLetter(lexer.char) {
+		lexer.readChar()
+	}
+
+	return lexer.input[position:lexer.position]
+}
+
+func (lexer *Lexer) readNumber() string {
+	position := lexer.position
+	for isDigit(lexer.char) {
+		lexer.readChar()
+	}
+
+	return lexer.input[position:lexer.position]
+}
+
+func (lexer *Lexer) skipWhitespace() {
+	for lexer.char == ' ' || lexer.char == '\t' || lexer.char == '\n' || lexer.char == '\r' {
+		lexer.readChar()
+	}
+}
+
+func (lexer *Lexer) peekChar() byte {
+	if lexer.readPosition >= len(lexer.input) {
+		return 0
+	} else {
+		return lexer.input[lexer.readPosition]
+	}
+}
+
+func newToken(tokenType token.Type, char byte) token.Token {
+	return token.Token{Type: tokenType, Literal: string(char)}
+}
+
+func isLetter(char byte) bool {
+	return 'a' <= char && char <= 'z' || 'A' <= char && char <= 'Z' || char == '_'
+}
+
+func isDigit(char byte) bool {
+	return '0' <= char && char <= '9'
+}
---
+++ token/token.go
@@ -0,0 +1,60 @@
+package token
+
+type Type string
+
+type Token struct {
+	Type    Type
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	IDENT = "IDENT"
+	INT   = "INT"
+
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	SLASH    = "/"
+	ASTERISK = "*"
+	LT       = "<"
+	GT       = ">"
+	EQ       = "=="
+	NOT_EQ   = "!="
+
+	SEMICOLON = ";"
+	COMMA     = ","
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
+)
+
+var keywords = map[string]Type{
+	"fn":     FUNCTION,
+	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
+}
+
+func LookupIdent(ident string) Type {
+	if t, ok := keywords[ident]; ok {
+		return t
+	}
+	return IDENT
+}
